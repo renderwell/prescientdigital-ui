6c2ec5045aa60ef5f43cbef18725e1dc4e7ca609
renderwell <renderwell@gmail.com>
2017-02-18 17:39:46 +0000

    Iconized cards, off-canvas menu, testimonial slider.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,18 +1,18 @@
 $(document).foundation();
 
 $(document).on('ready', function() {
-    $('.hero__slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        autoplay: true,
-        autoplaySpeed: 6000,
-        pauseOnHover: false,
-        dots: false,
-        arrows: false
-    });
+    // $('.hero__slider').slick({
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 500,
+    //     fade: true,
+    //     cssEase: 'linear',
+    //     autoplay: true,
+    //     autoplaySpeed: 6000,
+    //     pauseOnHover: false,
+    //     dots: false,
+    //     arrows: false
+    // });
 
     var s = skrollr.init({
         forceHeight: false
