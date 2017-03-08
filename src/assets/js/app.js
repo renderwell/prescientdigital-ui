359045157a9bb6bad437e4ea90bc21948edb7424
renderwell <renderwell@gmail.com>
2017-03-08 20:27:01 +0000

    Further work on home page and top-level landing page.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,20 +1,85 @@
 $(document).foundation();
 
 $(document).on('ready', function() {
-    // $('.hero__slider').slick({
-    //     dots: true,
-    //     infinite: true,
-    //     speed: 500,
-    //     fade: true,
-    //     cssEase: 'linear',
-    //     autoplay: true,
-    //     autoplaySpeed: 6000,
-    //     pauseOnHover: false,
-    //     dots: false,
-    //     arrows: false
-    // });
-
     var s = skrollr.init({
         forceHeight: false
     });
+
+    if (document.getElementById('stats-counter')) {
+        function startCounter() {
+            $('.counter').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+        }
+
+        var counter_started = false;
+
+        $(window).scroll(function(){
+            function elementScrolled(elem)
+            {
+                var docViewTop = $(window).scrollTop();
+                var docViewBottom = docViewTop + $(window).height();
+                var elemTop = $(elem).offset().top;
+                return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+            }
+
+            function elementScrolledIntoView(elem)
+            {
+                var docViewTop = $(window).scrollTop();
+                var docViewBottom = docViewTop + $(window).height();
+                var elemTop = $(elem).offset().top;
+                return elemTop <= docViewBottom;
+            }
+
+            if(elementScrolledIntoView('#stats-counter')) {
+                if (!counter_started) {
+                    startCounter();
+                    counter_started = true;
+                }
+            }
+        });
+    }
+
+    // $('.responsive').slick({
+    //   dots: true,
+    //   infinite: false,
+    //   speed: 300,
+    //   slidesToShow: 4,
+    //   slidesToScroll: 4,
+    //   responsive: [
+    //     {
+    //       breakpoint: 1024,
+    //       settings: {
+    //         slidesToShow: 3,
+    //         slidesToScroll: 3,
+    //         infinite: true,
+    //         dots: true
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 600,
+    //       settings: {
+    //         slidesToShow: 2,
+    //         slidesToScroll: 2
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 480,
+    //       settings: {
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1
+    //       }
+    //     }
+    //     // You can unslick at a given breakpoint now by adding:
+    //     // settings: "unslick"
+    //     // instead of a settings object
+    //   ]
+    // });
 });
