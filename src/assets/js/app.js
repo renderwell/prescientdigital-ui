301f28303c4972e30051b5253b40d98ea7b70ae2
renderwell <renderwell@gmail.com>
2017-05-07 15:10:29 +0100

    ui updates

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,25 @@
 $(document).foundation();
 
 $(document).on('ready', function() {
+    $('#reveal-search').click(function() {
+        $('#portal-searchbox').show('slow');
+        $('#searchGadget').focus();
+        $(this).hide();
+    });
+
+    $('#close-search').click(function() {
+        $('#portal-searchbox').hide();
+        $('#reveal-search').show();
+    });
+
+    $('.menu.vertical > li').hover(function() {
+        var el = $(this);
+        var dropdown_height = el.find('.submenu').height();
+        if (dropdown_height > $(window).height()) {
+            el.addClass('max');
+        }
+    });
+
     var s = skrollr.init({
         forceHeight: false
     });
@@ -12,7 +31,7 @@ $(document).on('ready', function() {
                 var counter_text = $this.siblings('.counter-text');
                 counter_text.addClass('hide-text');
                 $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                    duration: 4000,
+                    duration: 2000,
                     easing: 'swing',
                     step: function () {
                         $this.text(Math.ceil(this.Counter));
