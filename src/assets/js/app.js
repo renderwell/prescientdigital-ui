a8fa7a9dfcb4f8d432eff45fca9f811ec133aa5c
renderwell <renderwell@gmail.com>
2017-05-20 10:48:43 +0100

    Various UI mods.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,32 @@
-$(document).foundation();
 
 $(document).on('ready', function() {
+    $(document).foundation();
+
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    if(!isMobile.any()) {
+        console.log('MOBILE');
+    }
+
     $('#reveal-search').click(function() {
         $('#portal-searchbox').show('slow');
         $('#searchGadget').focus();
@@ -20,59 +46,64 @@ $(document).on('ready', function() {
         }
     });
 
-    var s = skrollr.init({
-        forceHeight: false
-    });
+    // Only use skrollr above viewport width 600px
+    // if (document.documentElement.clientWidth > 600) {
+    if(!isMobile.any()) {
+        console.log('SKROLLR LOADED');
+        var s = skrollr.init({
+            forceHeight: false
+        });
 
-    if (document.getElementById('stats-counter')) {
-        function startCounter() {
-            $('.counter').each(function () {
-                var $this = $(this);
-                var counter_text = $this.siblings('.counter-text');
-                counter_text.addClass('hide-text');
-                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.ceil(this.Counter));
-                    },
-                    complete: function() {
-                        counter_text.removeClass('hide-text');
-                    }
+        if (document.getElementById('stats-counter')) {
+            function startCounter() {
+                $('.counter').each(function () {
+                    var $this = $(this);
+                    var counter_text = $this.siblings('.counter-text');
+                    counter_text.addClass('hide-text');
+                    $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                        duration: 2000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.ceil(this.Counter));
+                        },
+                        complete: function() {
+                            counter_text.removeClass('hide-text');
+                        }
+                    });
                 });
+            }
+
+            var counter_started = false;
+
+            $(window).scroll(function(){
+                function elementScrolled(elem)
+                {
+                    var docViewTop = $(window).scrollTop();
+                    var docViewBottom = docViewTop + $(window).height();
+                    var elemTop = $(elem).offset().top;
+                    return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+                }
+
+                function elementScrolledIntoView(elem)
+                {
+                    var docViewTop = $(window).scrollTop();
+                    var docViewBottom = docViewTop + $(window).height();
+                    var elemTop = $(elem).offset().top;
+                    return elemTop <= docViewBottom;
+                }
+
+                if(elementScrolledIntoView('#stats-counter')) {
+                    if (!counter_started) {
+                        startCounter();
+                        counter_started = true;
+                    }
+                }
             });
         }
-
-        var counter_started = false;
-
-        $(window).scroll(function(){
-            function elementScrolled(elem)
-            {
-                var docViewTop = $(window).scrollTop();
-                var docViewBottom = docViewTop + $(window).height();
-                var elemTop = $(elem).offset().top;
-                return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
-            }
-
-            function elementScrolledIntoView(elem)
-            {
-                var docViewTop = $(window).scrollTop();
-                var docViewBottom = docViewTop + $(window).height();
-                var elemTop = $(elem).offset().top;
-                return elemTop <= docViewBottom;
-            }
-
-            if(elementScrolledIntoView('#stats-counter')) {
-                if (!counter_started) {
-                    startCounter();
-                    counter_started = true;
-                }
-            }
-        });
     }
 
     $('.feature-carousel').slick({
-      dots: true,
+      dots: false,
       infinite: true,
       speed: 300,
       slidesToShow: 3,
